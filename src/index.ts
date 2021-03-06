37077ace2ca03b5c781870397ad73a972f24401e
MrTeapot <axellarsson93@gmail.com>
2021-03-06 17:00:55 +0100

    Export catch and metadata

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ export { Service } from './decorators/Service'
 export { UseMiddleware } from './decorators/UseMiddleware'
 export { Endpoint, HTTPMethod } from './decorators/Endpoint';
 export { FastifyAdapter } from './http';
+export { Metadata } from './decorators/Metadata'
+export { Catch } from './decorators/Catch'
 export * from './interfaces';
 
 export * from './exceptions'
